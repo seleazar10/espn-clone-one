8fb4738fbd5efea3a3955bf885e91e9166a05578
selea <sve5040@gmail.com>
2021-12-13 21:08:46 -0600

    added BMI calculator

---
+++ script.js
@@ -0,0 +1,33 @@
+const now = 2021;
+const eleazarAge = now - 1991;
+
+console.log(now);
+console.log('Eleazar is ' + eleazarAge + ' years old');
+
+//name of subjects
+// const mark;
+// const john;
+
+//individual masses
+const johnMass = 78;
+const markMass = 92;
+
+//individual heights
+let johnHeight = 1.69;
+let markHeight = 1.95;
+
+johnHeight = 1;
+//equations for BMI
+const johnBMI = johnMass / (johnHeight * johnHeight);
+const markBMI = markMass / (markHeight * markHeight);
+
+//display data in console
+console.log("John's BMI is " + johnBMI);
+console.log("Mark's BMI is " + markBMI);
+
+console.log(`John BMI is ${johnBMI}`);
+
+console.log(`creating
+multi
+line
+elements`);
